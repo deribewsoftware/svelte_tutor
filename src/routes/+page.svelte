75851caa3d6e7d3df497519a6be0fc5e4228864d
deribew <deribewsoftware@gmail.com>
2024-01-14 17:07:27 +0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+
+
+<style>
+  h1{color: red;}
+  .main{
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    gap: 20px;
+
+  }
+  .main main{
+    flex-grow: 1;
+    background-color: aqua;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+</style>
+<div class="main">
+  <header>Navbar</header>
+  <main>
+    <div class="">hero</div>
+    
+
+  </main>
+  <footer>footer</footer>
+</div>
