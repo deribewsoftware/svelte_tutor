35ede9930bf14222736269e49e2a7a49308aead7
deribew <deribewsoftware@gmail.com>
2024-01-16 09:58:06 +0300

    hero added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,3 +34,7 @@
   </main>
   <footer>footer</footer>
 </div>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="50" height="50" fill="currentColor">
+  <circle cx="50" cy="50" r="40" stroke="red" stroke-width="3"  fill="blue"/>
+  <rect width="80" height="60" x="60" y="30" stroke="black"/>
+</svg>
